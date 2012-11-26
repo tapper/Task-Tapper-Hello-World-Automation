20d1a8a555b96f14cd90174464d29b7e82522157
Steffen Schwigon <ss5@renormalist.net>
2012-11-26 09:48:01 +0100

    initial commit

---
+++ lib/Task/Tapper/Hello/World/Automation.pm
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+package Task::Tapper::Hello::World::Automation;
+# ABSTRACT: Tapper - The easiest start for automation without hassle (hopefully)
+
+
+=pkgroup Tapper
+
+
+=pkg Task::Tapper::Hello::World
+
+=pkg Tapper::MCP
+
+=pkg Tapper::MCP::MessageReceiver
+
+1;
